6f5cd333ff4792c34090ff485b929e9b8d98a47e
Keenan Salandy <keenban@proton.me>
2025-07-11 19:20:47 -0400

    removed inhibit startup echo area message

--- early-init.el
+++ early-init.el
@@ -8,5 +8,4 @@
       inhibit-splash-screen t
       inhibit-startup-screen t
       inhibit-x-resources t
-      inhibit-startup-echo-area-message user-login-name ; read the docstring
       inhibit-startup-buffer-menu t)
