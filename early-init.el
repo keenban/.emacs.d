21ffb04b421dab1f3dbdf06eca45f52fb3a34441
Keenan Salandy <keenban@proton.me>
2025-07-11 19:19:54 -0400

    added early initialization section

---
+++ early-init.el
@@ -0,0 +1,12 @@
+(setq frame-resize-pixelwise t
+      frame-inhibit-implied-resize 'force
+      frame-title-format '("%b")
+      ring-bell-function 'ignore
+      use-dialog-box t ; only for mouse events, which I seldom use
+      use-file-dialog nil
+      use-short-answers t
+      inhibit-splash-screen t
+      inhibit-startup-screen t
+      inhibit-x-resources t
+      inhibit-startup-echo-area-message user-login-name ; read the docstring
+      inhibit-startup-buffer-menu t)
