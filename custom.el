14e9e4706d913d49592a0767bc3d67fd1a850c03
Keenan Salandy <keenban@proton.me>
2025-07-12 02:09:48 -0400

    added custom

---
+++ custom.el
@@ -0,0 +1,17 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-fold-core-style 'overlays)
+ '(package-selected-packages
+   '(calibre dash denote-org eat ellama ement emms exwm hyperbole kiwix
+	     magit markdown-mode nov oauth2 orderless pdf-tools
+	     pinentry s spacious-padding vertico yasnippet)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(fixed-pitch-serif ((t (:family "Serif")))))
