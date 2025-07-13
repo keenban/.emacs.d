7f918cf32af4e7657b8e85aee8315cf3e9421dd2
Keenan Salandy <keenban@proton.me>
2025-07-13 13:42:05 -0400

    added abbreviations to org config

--- keenban/keenban-org.el
+++ keenban/keenban-org.el
@@ -4,6 +4,8 @@
    ("C-c a" . org-agenda))
   :config
   (setq org-log-done t)
-  (setq org-agenda-files '("~/media/doc/notes/20250707T180240--agenda__agenda.org")))
+  (setq org-agenda-files '("~/media/doc/notes/20250707T180240--agenda__agenda.org"))
+  (define-abbrev org-mode-abbrev-table ";el" "#+BEGIN_SRC emacs-lisp")
+  (define-abbrev org-mode-abbrev-table ";end" "#+END_SRC"))
 
 (provide 'keenban-org)
