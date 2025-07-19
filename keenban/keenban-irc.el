a72eb7b995f1248fe0b90bbaa39b469104d1bd7a
Keenan Salandy <keenban@proton.me>
2025-07-19 19:08:28 -0400

    removed use-package, added require, removed custom funcs & keys

--- keenban/keenban-irc.el
+++ keenban/keenban-irc.el
@@ -1,29 +1,12 @@
-(use-package erc
-  :config
-  (setq erc-hide-list '("JOIN" "PART" "QUIT"))
-  
-  ;; use authentication
-  (add-to-list 'erc-modules 'services)
-  (setq erc-prompt-for-nickserv-password nil)
-  (setq erc-use-auth-source-for-nickserv-password t)
-  (add-to-list 'erc-nickserv-alist '(GGn nil nil "NickServ" "IDENTIFY" nil nil "Password accepted - you are now recognized."))
-  
-  (defun irc-join-ggn ()
-    (interactive)
-    (erc-tls :server "irc.gazellegames.net" :port +7000 :nick "keenban"))
+(require 'erc)
+;; hide annoying messages
+(setq erc-hide-list '("JOIN" "PART" "QUIT"))
 
-  (defun irc-join-red ()
-    (interactive)
-    (erc-tls :server "irc.scratch-network.net" :port +7000 :nick "keenban"))
-
-  (defun irc-join-mam ()
-    (interactive)
-    (erc-tls :server "irc.myanonamouse.net" :port 6697 :nick "keenban"))
-
-  :bind
-  (("C-c i g" . irc-join-ggn)
-   ("C-c i r" . irc-join-red)
-   ("C-c i m" . irc-join-mam)))
+;; use authentication
+(add-to-list 'erc-modules 'services)
+(setq erc-prompt-for-nickserv-password nil)
+(setq erc-use-auth-source-for-nickserv-password t)
+(add-to-list 'erc-nickserv-alist '(GGn nil nil "NickServ" "IDENTIFY" nil nil "Password accepted - you are now recognized."))
 
 (defun tmp/irc-login (network nick)
   (message "hook has been called")
