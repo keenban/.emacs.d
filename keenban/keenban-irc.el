d97d17e7c90fafb0c02ca7c65ebd8f723f5cef71
Keenan Salandy <keenban@proton.me>
2025-07-19 22:25:59 -0400

    cleaned up

--- keenban/keenban-irc.el
+++ keenban/keenban-irc.el
@@ -9,25 +9,13 @@
 ;; use nickserv automatically, with credentials loaded from ~/.authinfo.gpg
 (setq erc-prompt-for-nickserv-password nil)
 (setq erc-use-auth-source-for-nickserv-password t)
-(add-to-list 'erc-nickserv-alist '(GGn "NickServ!services@services.gazellegames.net" nil "NickServ" "IDENTIFY" nil nil "Password accepted - you are now recognized."))
+(add-to-list 'erc-nickserv-alist '(site "NickServ!services@services.site.net" nil "NickServ" "IDENTIFY" nil nil "Password accepted - you are now recognized."))
 
 (defun my/irc-login (network nick)
-  (when (eq network 'GGn)
+  (when (eq network 'site)
     ;; need to get password decrypted 
-    (erc-server-send (format "/msg Vertico ENTER #gazellegames %s" nick))))
+    (erc-server-send (format "/msg Bot ENTER #channel %s" nick))))
 
 (add-hook 'erc-nickserv-identified-hook 'my/irc-login)
-
-(defun irc-join-ggn ()
-  (interactive)
-  (erc-tls :server "irc.gazellegames.net" :port +7000 :nick "keenban"))
-
-(defun irc-join-red ()
-  (interactive)
-  (erc-tls :server "irc.scratch-network.net" :port +7000 :nick "keenban"))
-
-(defun irc-join-mam ()
-  (interactive)
-  (erc-tls :server "irc.myanonamouse.net" :port 6697 :nick "keenban"))
 
 (provide 'keenban-irc)
