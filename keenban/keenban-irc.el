6972b85078d37ede7fb8fc1ce154b88a79f5242f
Keenan Salandy <keenban@proton.me>
2025-07-19 22:18:46 -0400

    commented, and moved require erc services

--- keenban/keenban-irc.el
+++ keenban/keenban-irc.el
@@ -1,11 +1,12 @@
 (require 'erc)
-(require 'erc-services)
 
-;; hide annoying messages
 (setq erc-hide-list '("JOIN" "PART" "QUIT"))
 
-;; use authentication
+;; use services for authentication
+(require 'erc-services)
 (add-to-list 'erc-modules 'services)
+
+;; use nickserv automatically, with credentials loaded from ~/.authinfo.gpg
 (setq erc-prompt-for-nickserv-password nil)
 (setq erc-use-auth-source-for-nickserv-password t)
 (add-to-list 'erc-nickserv-alist '(GGn "NickServ!services@services.gazellegames.net" nil "NickServ" "IDENTIFY" nil nil "Password accepted - you are now recognized."))
