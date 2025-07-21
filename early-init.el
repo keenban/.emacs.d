1732754dd1890c566bcc366cc9edc0d7ef0b0d3c
Keenan Salandy <keenban@proton.me>
2025-07-21 10:58:20 -0400

    put it all in init.el and early-init.el

--- early-init.el
+++ early-init.el
@@ -1,9 +1,13 @@
-(setq inhibit-splash-screen t
-      inhibit-startup-screen t
-      inhibit-startup-message t
-      inhibit-startup-echo-area-message t
-      inhibit-startup-buffer-menu t
-      menu-bar-mode nil
-      tool-bar-mode nil
-      tooltip-mode nil)
-(set-scroll-bar-mode nil)
+;; When I start Emacs, I want it to be as blank as possible.
+;; This is because I find additional noise to distract from
+;; what I am immediately trying to do.
+  
+  (setq inhibit-splash-screen t
+        inhibit-startup-screen t
+        inhibit-startup-message t
+        inhibit-startup-echo-area-message t
+        inhibit-startup-buffer-menu t
+        menu-bar-mode nil
+        tool-bar-mode nil
+        tooltip-mode nil)
+  (set-scroll-bar-mode nil)
