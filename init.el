f6bcb7928b401ff66260a4f899bdabe6fb028acb
Keenan Salandy <keenansalandy@gmail.com>
2025-09-10 19:53:27 -0400

    removed all instances of use-package

--- init.el
+++ init.el
@@ -1,129 +1,118 @@
-(use-package emacs
-  :config
-  (setq epa-pinentry-mode 'loopback)
+(require 'package)
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
 
-  (setq custom-file (concat user-emacs-directory "custom.el"))
-  (load custom-file 'noerror)
+(defun ensure-package-installed (&rest packages)
+  (dolist (pkg packages)
+    (unless (package-installed-p pkg)
+      (package-install pkg))))
 
-  (set-face-attribute 'default nil :height 160)
-  (require-theme 'modus-themes)
-  (setq modus-themes-mixed-fonts t
-	modus-themes-prompts '(italic bold))
-  (setq	modus-themes-headings
-	(quote ((0 . (1.75))
-		(1 . (1.5))
-		(2 . (1.375))
-		(3 . (1.25))
-		(t . (1.125)))))
-  (setq modus-vivendi-tinted-palette-overrides
-	'((bg-mode-line-active bg-lavender)
-	  (bg-paren-match bg-magenta-intense)
-	  (bg-prose-block-contents bg-dim)
-          (bg-prose-block-delimiter bg-dim)
-          (fg-prose-block-delimiter fg-main)
-	  (underline-err red-faint)
-          (underline-warning yellow-faint)
-          (underline-note cyan-faint)
-	  (fg-heading-0 magenta-cooler)
-	  (fg-heading-1 magenta-cooler)
-	  (fg-heading-2 magenta-cooler)
-	  (fg-heading-3 magenta-cooler)
-	  (fg-heading-4 magenta-cooler)
-	  (fg-heading-5 magenta-cooler)
-	  (fg-heading-6 magenta-cooler)
-	  (fg-heading-7 magenta-cooler)
-	  (fg-heading-8 magenta-cooler)
-	  (comment yellow-faint)
-	  (string green-warmer)))
-	
-  (load-theme 'modus-vivendi-tinted t))
+(ensure-package-installed
+ 'vertico 'marginalia 'orderless 'consult
+ 'magit 'emms 'denote 'denote-journal)
 
-(use-package savehist
-  :hook after-init)
+(unless (package-installed-p 'nov)
+  (package-vc-install "https://depp.brause.cc/nov.el.git"))
 
-(use-package which-key
-  :hook after-init)
+(setq custom-file
+      (concat user-emacs-directory "custom.el"))
+(load custom-file 'noerror)
 
-(use-package vertico
-  :ensure t
-  :hook after-init)
+(setq epa-pinentry-mode 'loopback)
 
-(use-package marginalia
-  :ensure t
-  :hook after-init)
+(set-face-attribute 'default nil :height 160)
+(require-theme 'modus-themes)
+(setq modus-themes-mixed-fonts t modus-themes-prompts
+      '(italic bold))
+(setq modus-themes-headings
+      '((0 1.75) (1 1.5) (2 1.375) (3 1.25) (t 1.125)))
+(setq modus-vivendi-tinted-palette-overrides
+      '((bg-mode-line-active bg-lavender)
+	(bg-paren-match bg-magenta-intense)
+	(bg-prose-block-contents bg-dim)
+	(bg-prose-block-delimiter bg-dim)
+	(fg-prose-block-delimiter fg-main)
+	(underline-err red-faint)
+	(underline-warning yellow-faint)
+	(underline-note cyan-faint)
+	(fg-heading-0 magenta-cooler)
+	(fg-heading-1 magenta-cooler)
+	(fg-heading-2 magenta-cooler)
+	(fg-heading-3 magenta-cooler)
+	(fg-heading-4 magenta-cooler)
+	(fg-heading-5 magenta-cooler)
+	(fg-heading-6 magenta-cooler)
+	(fg-heading-7 magenta-cooler)
+	(fg-heading-8 magenta-cooler)
+	(comment yellow-faint) (string green-warmer)))
+(load-theme 'modus-vivendi-tinted t)
 
-(use-package orderless
-  :ensure t
-  :config
-  (setq completion-styles '(orderless basic)
-        completion-category-defaults nil
-        completion-category-overrides nil))
+(require 'savehist)
+(add-hook 'after-init-hook #'savehist-mode)
 
-(use-package consult
-  :ensure t
-  :bind (([remap switch-to-buffer] . consult-buffer)
-         ([remap count-lines-page] . consult-line)))
+(require 'which-key)
+(add-hook 'after-init-hook #'which-key-mode)
 
-(use-package org
-  :defer t
-  :mode ("\\.org\\'" . org-mode)
-  :bind (("C-c l" . org-store-link)
-         ("C-c a" . org-agenda))
-  :config
-  (setq org-startup-folded t
-	org-startup-indented t
-        org-insert-heading-respect-content t
-        org-hide-leading-stars t
-        org-log-done t
-        org-agenda-files '("~/media/doc/notes/20250707T180240--agenda.org")))
+(require 'vertico)
+(add-hook 'after-init-hook #'vertico-mode)
 
-(use-package org-capture
-  :bind ("C-c c" . org-capture)
-  :config
-  (setq org-capture-templates
-	'(("f" "Fleeting thoughts and ideas" entry
-	   (file+headline "~/media/doc/notes/20250909T140227--inbox__important.org" "Fleeting")
-	   "* %^{Title}\n:PROPERTIES:\n:CAPTURED: %U\n:CUSTOM_ID: h:%(format-time-string \"%Y%m%dT%H%M%S\")\n:END:\n\n%?"
-	   :empty-lines-after 1))))
+(require 'marginalia)
+(add-hook 'after-init-hook 'marginalia-mode)
 
-(use-package denote
-  :ensure t
-  :hook (dired-mode . denote-dired-mode)
-  :bind (("C-c n n" . denote)
-         ("C-c n r" . denote-rename-file)
-         ("C-c n l" . denote-link)
-         ("C-c n b" . denote-backlinks)
-         ("C-c n d" . denote-dired)
-         ("C-c n g" . denote-grep))
-  :config
-  (setq denote-directory (expand-file-name "~/media/doc/notes/")
-        denote-rename-confirmations nil)
-  (denote-rename-buffer-mode 1))
+(require 'orderless)
+(setq completion-styles '(orderless basic)
+      completion-category-defaults nil
+      completion-category-overrides nil)
 
-(use-package denote-journal
-  :ensure t
-  :commands (denote-journal-new-entry
-             denote-journal-new-or-existing-entry
-             denote-journal-link-or-create-entry)
-  :hook (calendar-mode . denote-journal-calendar-mode)
-  :bind (("C-c n j" . denote-journal-new-or-existing-entry))
-  :config
-  (setq denote-journal-directory (expand-file-name "journal" denote-directory)
-        denote-journal-keyword "journal"
-        denote-journal-title-format 'day-date-month-year))
+(require 'consult)
+(global-set-key [remap switch-to-buffer] 'consult-buffer)
+(global-set-key [remap count-lines-page] 'consult-line)
 
-(use-package magit
-  :ensure t)
+(require 'org)
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+(setq org-startup-folded t org-startup-indented t
+      org-insert-heading-respect-content t
+      org-hide-leading-stars t org-log-done t
+      org-agenda-files
+      '("~/media/doc/notes/20250707T180240--agenda.org"))
+(setq org-capture-templates
+'(("f" "Fleeting thoughts and ideas" entry
+   (file+headline
+    "~/media/doc/notes/20250909T140227--inbox__important.org"
+    "Fleeting")
+   "* %^{Title}\n:PROPERTIES:\n:CAPTURED: %U\n:CUSTOM_ID: h:%(format-time-string \"%Y%m%dT%H%M%S\")\n:END:\n\n%?"
+   :empty-lines-after 1)))
 
-(use-package emms
-  :ensure t
-  :config
-  (emms-all)
-  (setq emms-player-list '(emms-player-mpv)
-        emms-info-functions '(emms-info-native)))
+(require 'denote)
+(add-hook 'dired-mode-hook 'denote-dired-mode)
+(global-set-key (kbd "C-c n n") 'denote)
+(global-set-key (kbd "C-c n r") 'denote-rename-file)
+(global-set-key (kbd "C-c n l") 'denote-link)
+(global-set-key (kbd "C-c n b") 'denote-backlinks)
+(global-set-key (kbd "C-c n d") 'denote-dired)
+(global-set-key (kbd "C-c n g") 'denote-grep)
+(setq denote-directory (expand-file-name "~/media/doc/notes/")
+      denote-rename-confirmations nil)
+(denote-rename-buffer-mode 1)
 
-(use-package nov
-  :vc (:url "https://depp.brause.cc/nov.el.git")
-  :mode ("\\.epub\\'" . nov-mode)
-  :config
-  (setq nov-text-width 70))
+(require 'denote-journal)
+(add-hook 'calendar-mode-hook 'denote-journal-calendar-mode)
+(global-set-key (kbd "C-c n j") 'denote-journal-new-or-existing-entry)
+(setq denote-journal-directory (expand-file-name "journal" denote-directory)
+      denote-journal-keyword "journal"
+      denote-journal-title-format 'day-date-month-year)
+
+(require 'magit)
+
+(require 'emms-setup)
+(emms-all)
+(setq emms-player-list '(emms-player-mpv)
+      emms-info-functions '(emms-info-native))
+
+(setq nov-text-width 70)
+(add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
+(setq nov-text-width 70)
