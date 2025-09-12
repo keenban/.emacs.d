c4549c73ce3f2a1c4fcdaa4aee04b504bf3d1016
Keenan Salandy <keenansalandy@gmail.com>
2025-09-12 13:58:54 -0400

    stop emacs from littering with backups and auto saves

--- init.el
+++ init.el
@@ -14,6 +14,12 @@
  'vertico 'marginalia 'orderless 'consult
  'magit 'emms 'denote 'denote-journal 'bbdb 'nov)
 
+(setq backup-directory-alist
+      `(("." . ,(expand-file-name "tmp/backups/" user-emacs-directory))))
+(make-directory (expand-file-name "tmp/auto-saves/" user-emacs-directory) t)
+(setq auto-save-list-file-prefix (expand-file-name "tmp/auto-saves/sessions/" user-emacs-directory)
+      auto-save-file-name-transforms `((".*" ,(expand-file-name "tmp/auto-saves/" user-emacs-directory) t)))
+
 (setq custom-file
       (concat user-emacs-directory "custom.el"))
 (load custom-file 'noerror 'nomessage)
